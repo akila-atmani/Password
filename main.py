f5108a087a935eec7d7287c47846b9366c569057
akila-atmani <akila.atmani@laplateforme.io>
2024-11-27 08:13:46 +0100

    modification du code

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+liste_mdp = []
 mot_depasse=input("veuillez entrer votre mot de passe:")
-lettre_majuscule= "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z "
-lettre_miniscule= "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-for i in mot_depasse:
- if i < int(8):
-  print("votre mot de passe a moin de 8 caractréres.")
- elif i != int:
-  print("votre mot de passe doit contenir au moin un chiffre.")
- elif i != "!" or "@" or "#"or "%" or  "^" or "&" or "*":
-  print("votre mot de passe doit contenir au moin un caractére spécial.")
- elif i != lettre_majuscule:
-  print("votre mot de passe ne contient pas de majiscule.")
- elif i != lettre_miniscule:
-  print(" votre mot de passe ne contient pas de miniscule.")
+liste_mdp.append(mot_depasse)
+
+lettre_majuscule= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lettre_miniscule= "abcdefghijklmnopqrstuvwxyz"
+
+
+if len(mot_depasse) <= 8:
+  print("votre mot de passe a moin de 8 caractréres, veuillez réessayer de nouveau.") 
+  input("nouveau mot de passe:")
+
+
+if  not any (caractere.isdigit() for caractere in mot_depasse):
+ print("votre mot de passe doit contenir au moin un chiffre, veuillez réessayer de nouveau.")
+ input("nouveau mot de passe:")
+
+
+if not (mot_depasse.__contains__("!") or 
+        mot_depasse.__contains__("@") or 
+        mot_depasse.__contains__("#") or 
+        mot_depasse.__contains__("%") or 
+        mot_depasse.__contains__("^") or 
+        mot_depasse.__contains__("&") or 
+        mot_depasse.__contains__("*")):
+ print("Le mot de passe ne contient pas de caractère spécial.")
+ input("nouveau mot de passe:")
+
+
+
+if not any (char in mot_depasse for char in lettre_majuscule):
+  print("votre mot de passe ne contient pas de majiscule, veuillez réessayer de nouveau")
+  input("nouveau mot de passe:")
+
+
+if not any (char in mot_depasse for char in lettre_miniscule):
+  print(" votre mot de passe ne contient pas de miniscule,veuillez réessayer de nouveau")
+  input("nouveau mot de passe:")
+
+
+else :
+  print("votre mot de passe est correct")
